bad59049405fa580fa58dfdd7057a6e78fc453db
Chinedu ThankGod Ugwu <ugwuthankgodchinedu1@gmail.com>
2025-11-07 22:54:02 +0100

    Learning basic operations with python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Assign values to variables
+number1 = 10
+number2 = 5
+
+# Perform basic arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+
+
